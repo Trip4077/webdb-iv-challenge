35ef7df0227e61fe3015b80baa6d0ffdb38bcb9a
Trip4077 <bj2336@email.vccs.edu>
2019-03-21 11:31:50 -0400

    Recipe Schema Created

--- data/migrations/20190321112312_RECIPE_TABLE.js
+++ data/migrations/20190321112312_RECIPE_TABLE.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipes', tbl => {
+      tbl.increments();
+      tbl.integer('dish_id')
+         .notNullable();
+      tbl.string('recipe_name')
+         .notNullable()
+         .unique();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('recipes');
 };
