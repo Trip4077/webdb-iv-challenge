bc12a8aea9a20d221203c57ab5a5800700416f92
Trip4077 <bj2336@email.vccs.edu>
2019-03-21 11:34:56 -0400

    Ingredients Schema Created

--- data/migrations/20190321112407_INGREDIENT_TABLE.js
+++ data/migrations/20190321112407_INGREDIENT_TABLE.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('ingredients', tbl => {
+      tbl.increments();
+      tbl.string('ing_name')
+         .notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('ingredients');
 };
