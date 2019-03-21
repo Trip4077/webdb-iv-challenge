30395508576cc79eadb7c723281c97a65ab6242e
Trip4077 <bj2336@email.vccs.edu>
2019-03-21 12:49:57 -0400

    Get All Dishes and Get All Recipes Complete

--- data/dbHelpers.js
+++ data/dbHelpers.js
@@ -1,18 +1,15 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+
+const db = knex(knexConfig.development);
+
 module.exports = {
-
+    //Get All Dishes
     getDishes: () => {
-
+        return db('dish')
     },
-
+    //Get All Recipes
     getRecipes: () => {
-
+        return db('recipes')
     },
-
-    getIngredients: () => {
-
-    },
-
-    getMeasurements: () => {
-
-    }
 }
